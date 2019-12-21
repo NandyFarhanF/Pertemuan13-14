169106c162c6551b6b9321c873064dd25573b636
NandyFarhanF <farrizal28@gmail.com>
2019-12-21 11:51:44 +0700

    Pertemuan13-14

---
+++ 4 - Memahami program/src/pkg4/memahami/program/OtongSurotong.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg4.memahami.program;
+
+/**
+ *
+ * @author Nandy Farhan Fahrizal
+ */
+public class OtongSurotong {
+    public static void main(String[] args){
+
+		System.out.print("Nama ku adalah Otong, teman si ucup");
+
+	}
+    
+}
